6a0f8a29fd0c126f8b0b046b0051882bc28e4a88
mdm <80916664+mdm224@users.noreply.github.com>
2024-02-22 07:43:11 -0500

    Update tmdb.js
    ran through prettier

--- commands/tmdb.js
+++ commands/tmdb.js
@@ -1,137 +1,152 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function handleTmdb(message, arg) {
-    
-        let year;
-        const yearMatch = arg.match(/(\d{4})$/);
-    if (yearMatch) {
-        year = parseInt(yearMatch[0]);
-        arg = arg.replace(yearMatch[0], '').trim();
-    }
-        
-        [type, title] = arg.split(/\s+(.+)/).filter(Boolean);
-        
-        console.log('Type:', type);
-        console.log('Title:', title);
-        console.log('Year:', year);
+  let year;
+  const yearMatch = arg.match(/(\d{4})$/);
+  if (yearMatch) {
+    year = parseInt(yearMatch[0]);
+    arg = arg.replace(yearMatch[0], "").trim();
+  }
 
-        async function fetchDetails(title, type) {
-            const apiKey = '';
-            const baseUrl = 'https://api.themoviedb.org/3';
-            console.log('Type:', type);
-        console.log('Title:', title);
-            const searchUrl = `${baseUrl}/search/${type}?api_key=${apiKey}&query=${encodeURIComponent(title)}`;
-        
-            if (type === "movie"){
-            try {
-                const response = await axios.get(searchUrl);
-                var data = response.data;
-        
-                if (data.results && data.results.length > 0) {
-                    let movieId;
-                        if (year) {
-                            const movieWithYear = data.results.find(movie => movie.release_date && new Date(movie.release_date).getFullYear() === year);
-                        if (movieWithYear) {
-                            movieId = movieWithYear.id;
-                        } else {
-                            return 'No results found for the provided title and year.';
-                            }
-                    } else {
-                    movieId = data.results[0].id;
-                }
+  [type, title] = arg.split(/\s+(.+)/).filter(Boolean);
 
-                    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`);
-                    data = await response.json();
-                    const overview = data.overview;
-                    const release = data.release_date ? new Date(data.release_date).getFullYear() : 'N/A';
-                    const imgUrl = data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : 'N/A';
-                    const rating = data.vote_average;
-                    const votes = data.vote_count;
-                    const runtime = data.runtime + ' minutes';
-                    var genresArray = data.genres.map(genre => genre.name);
+  console.log("Type:", type);
+  console.log("Title:", title);
+  console.log("Year:", year);
 
-                    console.log('TMDB API Response:', data);
-                    message.reply(`/adduhtml Info,
+  async function fetchDetails(title, type) {
+    const apiKey = "";
+    const baseUrl = "https://api.themoviedb.org/3";
+    console.log("Type:", type);
+    console.log("Title:", title);
+    const searchUrl = `${baseUrl}/search/${type}?api_key=${apiKey}&query=${encodeURIComponent(title)}`;
+
+    if (type === "movie") {
+      try {
+        const response = await axios.get(searchUrl);
+        var data = response.data;
+
+        if (data.results && data.results.length > 0) {
+          let movieId;
+          if (year) {
+            const movieWithYear = data.results.find(
+              (movie) =>
+                movie.release_date &&
+                new Date(movie.release_date).getFullYear() === year,
+            );
+            if (movieWithYear) {
+              movieId = movieWithYear.id;
+            } else {
+              return "No results found for the provided title and year.";
+            }
+          } else {
+            movieId = data.results[0].id;
+          }
+
+          const response = await fetch(
+            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`,
+          );
+          data = await response.json();
+          const overview = data.overview;
+          const release = data.release_date
+            ? new Date(data.release_date).getFullYear()
+            : "N/A";
+          const imgUrl = data.poster_path
+            ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
+            : "N/A";
+          const rating = data.vote_average;
+          const votes = data.vote_count;
+          const runtime = data.runtime + " minutes";
+          var genresArray = data.genres.map((genre) => genre.name);
+
+          console.log("TMDB API Response:", data);
+          message.reply(`/adduhtml Info,
                     <div style="float:left; margin-right:10px; overflow: scroll;">
                         <img src="${imgUrl}" width="200" height="225">
                     </div> 
                         <div style="border: 5px solid gray; border-radius: 10px; padding: 10px; height: 195px; overflow: scroll;">
                         <strong>Overview:</strong><br>${overview}<br>
-                        <strong>Genres:</strong> ${genresArray.join(', ')}<br>
+                        <strong>Genres:</strong> ${genresArray.join(", ")}<br>
                         <strong>Released:</strong> ${release}<br>
                         <strong>Runtime:</strong> ${runtime}<br>
                         <strong>Rating:</strong> ${rating}<br>
                         <strong>Number of votes:</strong> ${votes}
                     </div>`);
-                        return;
-                } else {
-                    return 'No results found for the provided title.';
-                }
-            } catch (error) {
-                console.error('Error fetching data from TMDB:', error.message);
-                return 'Error fetching data from TMDB.';
-            }
-            
+          return;
+        } else {
+          return "No results found for the provided title.";
         }
-            else{
-                try {
-                    const response = await axios.get(searchUrl);
-                    var data = response.data;
-            
-                    if (data.results && data.results.length > 0) {
-                            let showId;
-                                if (year) {
-                                    const showWithYear = data.results.find(show => show.first_air_date && new Date(show.first_air_date).getFullYear() === year);
-                                if (showWithYear) {
-                                    showId = showWithYear.id;
-                                } else {
-                                    return 'No results found for the provided title and year.';
-                                    }
-                            } else {
-                            showId = data.results[0].id;
-                        }
+      } catch (error) {
+        console.error("Error fetching data from TMDB:", error.message);
+        return "Error fetching data from TMDB.";
+      }
+    } else {
+      try {
+        const response = await axios.get(searchUrl);
+        var data = response.data;
 
-                        const response = await fetch(`https://api.themoviedb.org/3/tv/${showId}?api_key=${apiKey}&language=en-US`);
-                        data = await response.json();
-                        const overview = data.overview;
-                        const release = data.first_air_date ? new Date(data.first_air_date).getFullYear() : 'N/A';
-                        const imgUrl = data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : 'N/A';
-                        const rating = data.vote_average;
-                        const votes = data.vote_count;
-                        const seasons = data.number_of_seasons;
-                        var genresArray = data.genres.map(genre => genre.name);
-                        
-                       // console.log("Genres:", genresArray);
+        if (data.results && data.results.length > 0) {
+          let showId;
+          if (year) {
+            const showWithYear = data.results.find(
+              (show) =>
+                show.first_air_date &&
+                new Date(show.first_air_date).getFullYear() === year,
+            );
+            if (showWithYear) {
+              showId = showWithYear.id;
+            } else {
+              return "No results found for the provided title and year.";
+            }
+          } else {
+            showId = data.results[0].id;
+          }
 
-                        console.log('TMDB API Response:', data);
+          const response = await fetch(
+            `https://api.themoviedb.org/3/tv/${showId}?api_key=${apiKey}&language=en-US`,
+          );
+          data = await response.json();
+          const overview = data.overview;
+          const release = data.first_air_date
+            ? new Date(data.first_air_date).getFullYear()
+            : "N/A";
+          const imgUrl = data.poster_path
+            ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
+            : "N/A";
+          const rating = data.vote_average;
+          const votes = data.vote_count;
+          const seasons = data.number_of_seasons;
+          var genresArray = data.genres.map((genre) => genre.name);
 
-                        message.reply(`/adduhtml Info,
+          // console.log("Genres:", genresArray);
+
+          console.log("TMDB API Response:", data);
+
+          message.reply(`/adduhtml Info,
                         <div style="float:left; margin-right:10px; overflow: scroll;">
                         <img src="${imgUrl}" width="200" height="225">
                     </div> 
                         <div style="border: 5px solid gray; border-radius: 10px; padding: 10px; height: 195px; overflow: scroll;">
                         <strong>Overview:</strong><br>${overview}<br>
-                        <strong>Genres:</strong> ${genresArray.join(', ')}<br>
+                        <strong>Genres:</strong> ${genresArray.join(", ")}<br>
                         <strong>Released:</strong> ${release}<br>
                         <strong>Seasons:</strong> ${seasons}<br>
                         <strong>Rating:</strong> ${rating}<br>
                         <strong>Number of votes:</strong> ${votes}
                     </div>`);
-                        return;
-                    } else {
-                        return 'No results found for the provided title.';
-                    }
-            } catch (error) {
-                    console.error('Error fetching data from TMDB:', error.message);
-                    return 'Error fetching data from TMDB.';
-                }
-            }
+          return;
+        } else {
+          return "No results found for the provided title.";
+        }
+      } catch (error) {
+        console.error("Error fetching data from TMDB:", error.message);
+        return "Error fetching data from TMDB.";
+      }
     }
-        
-            fetchDetails(title, type);
-        
-}
+  }
 
+  fetchDetails(title, type);
+}
 
 handleTmdb.requiresArgs = true;
 module.exports = handleTmdb;
