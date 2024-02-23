3fe7532281aaae597d263e0b3bfbd7f6d74bbe27
mdm <80916664+mdm224@users.noreply.github.com>
2024-02-23 10:29:07 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,9 @@
 async function handleHelp(message, arg) {
-    return message.reply('!code Current commands are: \nrandgraycie - posts a random picture of graycie \nrandskip - posts a random picture of skipper \nrandcapy - posts a random picture of a capybara \nping - responds pong!  \nmetal - provides the metal-archives link of the requested band \ntmdb - provides the overview, release year, rating, and poster of the request show/movies. specify if show or movie in command such as -tmdb tv spongebob squarepants or -tmdb movie barbie');
+    return message.reply('!code Current commands are: '+ 
+    '\nrand(enterAnimal): posts a random picture of the requested animal. Current options include: Graycie, Skip, Capy, Ferret, Axolotl, Leopard, Raccoon, and Panda\n'+
+    '\nmetal: provides the metal-archives link of the requested band\n'+ 
+    '\ntmdb: Provides the overview, release date, genre, rating, and number of rates for TV shows and movies. TV shows also return the amount of seasons, and movies return the runtime.'+
+    '\nexample usage: -tmdb tv better call saul or -tmdb movie star wars: revenge of the sith. You can also (optionally) include the year as a way to specify which result you would like, if there are multiple of the same name. Year goes after the title (-tmdb tv avatar the last airbender 2005)');
 }
 
 handleHelp.requiresArgs = false;
