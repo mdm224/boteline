c4bcc023e5ff32642a78e8f22af039d65189397c
mdm <80916664+mdm224@users.noreply.github.com>
2024-02-29 11:53:58 -0500

    Update randskip.js

--- commands/randskip.js
+++ commands/randskip.js
@@ -1,10 +1,10 @@
 async function handleRandskip(message) {
-        const skips = 34;
+        const skips = 61;
 
         const Response = Math.floor(Math.random() * skips + 1);
             
 
-       const url = 'https://mdm-pics.com/Skipper/Skipper'+ Response + '.jpg';
+       const url = 'https://mdm-pics.com/Skipper/Skipper%20('+ Response + ').jpg';
        return message.reply("!show "+ url);
 }
 
