6d074826c5796bf06693f9c821e61c03f7b00e8c
mdm <80916664+mdm224@users.noreply.github.com>
2024-02-29 11:54:45 -0500

    Create randturtle.js

---
+++ commands/randturtle.js
@@ -0,0 +1,11 @@
+async function handleRandturtle(message) {
+    const turtles = 618;
+    const Response = Math.floor(Math.random() * turtles + 1);
+  
+   const url = 'https://mdm-pics.com/turtle/turtle%20('+ Response + ').jpg';
+   return message.reply("!show "+ url);
+}
+
+
+handleRandturtle.requiresArgs = false;
+module.exports = handleRandturtle;
