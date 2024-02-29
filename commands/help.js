703353f95832311dbf9f7911354737bdd87e5c14
mdm <80916664+mdm224@users.noreply.github.com>
2024-02-29 11:56:41 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 async function handleHelp(message, arg) {
     return message.reply('!code Current commands are: '+ 
-    '\nrand(enterAnimal): posts a random picture of the requested animal. Current options include: Graycie, Skip, Capy, Ferret, Axolotl, Leopard, Raccoon, and Panda\n'+
-    '\nmetal: provides the metal-archives link of the requested band\n'+ 
+    '\nrand(enterAnimal): posts a random picture of the requested animal. Current options include: Graycie, Skip, Capy, Ferret, Axolotl, Leopard, Raccoon, Turtle, Fish, tt (Down the Tubes 2 cats), Deli, and Panda\n'+
+    '\nYou can also just use randanimal itself as a command, and it will randomly select one of the options listed above and post an image of the selected animal/pet\n' + 
     '\ntmdb: Provides the overview, release date, genre, rating, and number of rates for TV shows and movies. TV shows also return the amount of seasons, and movies return the runtime.'+
     '\nexample usage: -tmdb tv better call saul or -tmdb movie star wars: revenge of the sith. You can also (optionally) include the year as a way to specify which result you would like, if there are multiple of the same name. Year goes after the title (-tmdb tv avatar the last airbender 2005)');
 }
