ebdcf82985ad7abe8d3827f915585e4819dd4c17
mdm <80916664+mdm224@users.noreply.github.com>
2024-03-04 09:28:10 -0500

    Create randredpanda.js

---
+++ commands/randredpanda.js
@@ -0,0 +1,12 @@
+async function handleRandredpanda(message) {
+    const redPands = 520;
+    const Response = Math.floor(Math.random() * redPands + 1);
+            
+
+    const url = 'https://mdm-pics.com/redPanda/redPanda%20('+ Response + ').jpg';
+    return message.reply("!show "+ url);
+}
+
+
+handleRandredpanda.requiresArgs = false;
+module.exports = handleRandredpanda;
