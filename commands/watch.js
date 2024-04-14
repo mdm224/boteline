b1a069d39a4004a90b6e26c6f9f743056b757e99
mdm <80916664+mdm224@users.noreply.github.com>
2024-04-14 14:42:47 -0400

    Update watch.js

--- commands/watch.js
+++ commands/watch.js
@@ -21,6 +21,9 @@ if (argParts.length > 1) {
 if (argParts.length > 2) {
   region = argParts[2].trim().toUpperCase();
 }
+else {
+  region = "US";
+}
 
 console.log("Type:", type);
 console.log("Title:", title);
@@ -29,7 +32,7 @@ console.log("Region:", region);
 
 
   async function fetchDetails(title, type) {
-    const apiKey = "1b7797bda0882359202e6bc898084d1e";
+    const apiKey = "";
     const baseUrl = "https://api.themoviedb.org/3";
     console.log("Type:", type);
     console.log("Title:", title);
@@ -50,22 +53,34 @@ console.log("Region:", region);
 
           );
           data2 = await response2.json();
-          const buy = data2.results.US !== undefined ? data2.results.US.buy : undefined;
+          console.log(data2.results[region]);
+          const buy = data2.results[region] !== undefined ? data2.results[region].buy : undefined;
           let buyProviders = buy !== undefined ? buy.map(b => b.provider_name) : "N/A";
           buyProviders = JSON.stringify(buyProviders);
           buyProviders = buyProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
+
+          const rent = data2.results[region] !== undefined ? data2.results[region].rent : undefined;
+          let rentProviders = buy !== undefined ? rent.map(b => b.provider_name) : "N/A";
+          rentProviders = JSON.stringify(rentProviders);
+          rentProviders = rentProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
+
+          const free = data2.results[region] !== undefined ? data2.results[region].free : undefined;
+          let freeProviders = free !== undefined ? free.map(free => free.provider_name) : "N/A";
+          freeProviders = JSON.stringify(freeProviders);
+          freeProviders = freeProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
+          console.log(freeProviders);
  
           const flatrate = data2.results[region] !== undefined ? data2.results[region].flatrate : undefined;
           let flatProviders = flatrate !== undefined ? flatrate.map(flatrate => flatrate.provider_name) : "N/A";
-         flatProviders = JSON.stringify(flatProviders);
-         flatProviders = flatProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
-          message.reply(`/adduhtml Streaming, ${title} can be streamed or purchased through the following platforms: <br><br> <Strong>Flatrate streaming:</Strong> ${flatProviders} <br> <Strong>Buying:</Strong> ${buyProviders} <br><br> Data provided by <a href="https://www.justwatch.com">JustWatch</a>!`);
-           return;
+          flatProviders = JSON.stringify(flatProviders);
+          flatProviders = flatProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
+          message.reply(`/adduhtml ${title}_${region}, <div style="background: linear-gradient(to right, #1a0659, #00b3ff); padding: 10px; border-radius: 5px; color: white;">${title} can be streamed or purchased through the following platforms: <br><br> <Strong>Free streaming: </Strong> ${freeProviders} <br> <Strong>Subscription streaming:</Strong> ${flatProviders} <br> <Strong>Renting:</Strong> ${rentProviders} <br> <Strong>Buying:</Strong> ${buyProviders} <br><br> Data provided by <a href="https://www.justwatch.com" style="color: white;">JustWatch</a>! (To see prices, check <a href="https://www.themoviedb.org/movie/${movieId}/watch?translate=false&locale=${region}"  style="color: white;">TMDB</a>!)</div>`);
+
         } else {
           return "No results found for the provided title.";
         }
       } catch (error) {
-        console.error("Error fetching data from TMDB:", error.message);
+        console.error(title + "Error fetching data from TMDB:", error.message);
         return "Error fetching data from TMDB.";
       }
     } else {
@@ -85,9 +100,17 @@ console.log("Region:", region);
 
           );
          data2 = await response2.json();
-        //console.log(data2.results);
+        console.log(data2.results);
          const buy = data2.results[region] !== undefined ? data2.results[region].buy : undefined;
          //console.log(buy);
+
+        const free = data2.results[region] !== undefined ? data2.results[region].free : undefined;
+        let freeProviders = free !== undefined ? free.map(free => free.provider_name) : "N/A";
+        freeProviders = JSON.stringify(freeProviders);
+        freeProviders = freeProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
+        console.log(freeProviders);
+          
+
          let buyProviders = buy !== undefined ? buy.map(b => b.provider_name) : "N/A";
          buyProviders = JSON.stringify(buyProviders);
          buyProviders = buyProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
@@ -98,13 +121,14 @@ console.log("Region:", region);
          flatProviders = flatProviders.replace(/[\[\]"]/g, '').replace(/,/g, ', ');
 
 
-         message.reply(`/adduhtml Streaming, ${title} can be streamed or purchased through the following platforms: <br><br> <Strong>Flatrate streaming:</Strong> ${flatProviders} <br> <Strong>Buying:</Strong> ${buyProviders} <br><br> Data provided by <a href="https://www.justwatch.com">JustWatch</a>!`);
+         message.reply(`/adduhtml ${title}_${region}, <div style="background: linear-gradient(to right, #1a0659, #00b3ff); padding: 10px; border-radius: 5px; color: white;">${title} can be streamed or purchased through the following platforms: <br><br> <Strong>Free streaming: </Strong> ${freeProviders} <br> <Strong>Subscription streaming:</Strong> ${flatProviders} <br> <Strong>Buying:</Strong> ${buyProviders} <br><br> Data provided by <a href="https://www.justwatch.com" style="color: white;">JustWatch</a>!  (To see prices, check <a href="https://www.themoviedb.org/tv/${showId}/watch?translate=false&locale=${region}"  style="color: white;">TMDB</a>!)</div>`);
+
           return;
         } else {
           return "No results found for the provided title.";
         }
       } catch (error) {
-        console.error("Error fetching data from TMDB:", error.message);
+        console.error(title + "Error fetching data from TMDB:", error.message);
         return "Error fetching data from TMDB.";
       }
     }
