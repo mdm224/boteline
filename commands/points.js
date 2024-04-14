e846461493a4cfb66a442c48be785a1bec91462b
mdm <80916664+mdm224@users.noreply.github.com>
2024-04-14 14:42:03 -0400

    Create points.js

---
+++ commands/points.js
@@ -0,0 +1,52 @@
+async function handlePoints(message, arg) {
+    if (!handlePoints.users) {
+        handlePoints.users = [];
+    }
+
+    if (arg === "print") {
+        const replyMessage = handlePoints.users.map(user => `${user.name}: ${user.points}`).join('<br>');
+        console.log(handlePoints.users);
+        if (!handlePoints.users.length)
+        {
+            message.reply("/adduhtml Leaderboard, <Strong>Current leaderboard:</Strong> <br> Nobody!");
+        }
+        else{
+        message.reply("/adduhtml Leaderboard, <Strong>Current leaderboard:</Strong> <br>" + replyMessage);
+        }
+        return;
+    }
+
+    if (arg === 'clear') {
+        handlePoints.users = [];
+        message.reply("Points cleared!");
+        return;
+    }
+
+
+    if (arg.includes(':')) {
+        const [user, pointsStr] = arg.split(':');
+        const points = parseInt(pointsStr);
+
+        const userIndex = handlePoints.users.findIndex(u => u.name === user);
+        if (userIndex !== -1) {
+            handlePoints.users[userIndex].points += points;
+        } else {
+            handlePoints.users.push({ name: user, points });
+        }
+
+        message.reply('Points updated!');
+        return;
+    }
+
+    const userIndex = handlePoints.users.findIndex(user => user.name === arg);
+    if (userIndex !== -1) {
+        handlePoints.users[userIndex].points += 1;
+    } else {
+        handlePoints.users.push({ name: arg, points: 1 });
+    }
+
+    message.reply('Points updated!');
+}
+
+handlePoints.requiresArgs = true;
+module.exports = handlePoints;
